eb8d60948d368a1752ef50947a442243bca10232
Adriean Khisbe <adriean.khisbe@live.fr>
2015-04-30 11:15:27 +0200

    refactor: john and chuck joining the magic constant party

--- src/main/test/java/org/craftedsw/harddependancies/trip/TripServiceTest.java
+++ src/main/test/java/org/craftedsw/harddependancies/trip/TripServiceTest.java
@@ -17,12 +17,13 @@ import static org.assertj.core.api.Assertions.*;
  */
 public class TripServiceTest {
 
+    private static User JOHN = new User();
+    private static User CHUCK = new User();
 
     private User loggedUser;
     private User userArg;
     private TripService tripService;
     private List<Trip> daoTripResult;
-
 
     @Before
     public void setUp() throws Exception {
@@ -42,8 +43,8 @@ public class TripServiceTest {
     @Test
     public void getTrip_shouldReturnNoTrip_whenGivenUSerHasNoFriend() throws Exception {
         //given
-        loggedUser = new User();
-        userArg = new User();
+        loggedUser = JOHN;
+        userArg = CHUCK;
 
         // then
         List<Trip> trips = tripService.getTripsByUser(userArg);
@@ -53,8 +54,8 @@ public class TripServiceTest {
     @Test
     public void getTrip_shouldGiveTheTripOfTheLoggedUser_whenGivenUserIsFriendOFLoggedInUser() throws Exception {
         //given
-        loggedUser = new User();
-        userArg = new User();
+        loggedUser = JOHN;
+        userArg = CHUCK;
         userArg.getFriends().add(loggedUser);
 
         daoTripResult = new ArrayList<Trip>();
@@ -70,8 +71,9 @@ public class TripServiceTest {
     @Test
     public void getTrip_shouldNoTrips_whenGivenUSerIsNoFriendOfLoggedUser() throws Exception {
         //given
-        loggedUser = new User();
-        userArg = new User();
+        loggedUser = JOHN;
+        userArg = CHUCK;
+
         for (int i = 0; i < 3; i++)
             userArg.addFriend(new User());
 
